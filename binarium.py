9a216862ed851bd6924cabc22d51d8e4f925c5fd
Barlitos <karolb156@gmail.com>
2020-09-14 12:24:06 +0200

    nowe rzeczy

--- binarium.py
+++ binarium.py
@@ -1,12 +1,9 @@
 print("\n           BINARIUM IN PYTHON")
 print("     --your converter with steps--")
 variable = input("Your number: ")
-#bin(variable)
-#index = int(index)
-#for i in range(string[index:]): 
 
 #binar = bin(variable)
 print("\tbinary: {0:b}".format(int(variable)))
-#print(len(bin(int(variable)))))
-print("\t\t---\n\t\t421 -> 4*1 +2*1+1*0 = 6 ")
+#print(len(bin(int(variable)))))print("\t\t---\n\t\t421 -> 4*1 +2*1+1*0 = 6 ")
 
+print ("eluwina")
