ba3414462806a26eff9a0167772df4462e08b367
Barlitos <karolb156@gmail.com>
2020-09-14 12:29:28 +0200

    new loop

--- binarium.py
+++ binarium.py
@@ -2,8 +2,9 @@ print("\n           BINARIUM IN PYTHON")
 print("     --your converter with steps--")
 variable = input("Your number: ")
 
-#binar = bin(variable)
 print("\tbinary: {0:b}".format(int(variable)))
+
 #print(len(bin(int(variable)))))print("\t\t---\n\t\t421 -> 4*1 +2*1+1*0 = 6 ")
 
-print ("eluwina")
+for i in range(0,10):
+    print ("eluwina",i)
